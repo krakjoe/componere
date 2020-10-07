5fd7516f2f57160d1f1cdc39c1d0598e4a6dd8c1
Remi Collet <remi@remirepo.net>
2020-10-07 10:35:30 +0200

    generate arginfo for Componere\Method

---
+++ src/method.stub.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @generate-function-entries
+ * @generate-legacy-arginfo
+ */
+
+namespace Componere;
+
+class Method {
+
+	public function __construct(\Closure $closure) {}
+
+	public function setProtected():Method {}
+
+	public function setPrivate():Method {}
+
+	public function setStatic():Method {}
+
+	public function setFinal():Method {}
+
+	public function getReflector():\ReflectionMethod {}
+}
---
+++ src/method_legacy_arginfo.h
@@ -0,0 +1,36 @@
+/* This is a generated file, edit the .stub.php file instead.
+ * Stub hash: bec40f676dddd349354623c052007d03fd5bd97b */
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_class_Method___construct, 0, 0, 1)
+	ZEND_ARG_INFO(0, closure)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_class_Method_setProtected, 0, 0, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_class_Method_setPrivate arginfo_class_Method_setProtected
+
+#define arginfo_class_Method_setStatic arginfo_class_Method_setProtected
+
+#define arginfo_class_Method_setFinal arginfo_class_Method_setProtected
+
+#define arginfo_class_Method_getReflector arginfo_class_Method_setProtected
+
+
+ZEND_METHOD(Method, __construct);
+ZEND_METHOD(Method, setProtected);
+ZEND_METHOD(Method, setPrivate);
+ZEND_METHOD(Method, setStatic);
+ZEND_METHOD(Method, setFinal);
+ZEND_METHOD(Method, getReflector);
+
+
+static const zend_function_entry class_Method_methods[] = {
+	ZEND_ME(Method, __construct, arginfo_class_Method___construct, ZEND_ACC_PUBLIC)
+	ZEND_ME(Method, setProtected, arginfo_class_Method_setProtected, ZEND_ACC_PUBLIC)
+	ZEND_ME(Method, setPrivate, arginfo_class_Method_setPrivate, ZEND_ACC_PUBLIC)
+	ZEND_ME(Method, setStatic, arginfo_class_Method_setStatic, ZEND_ACC_PUBLIC)
+	ZEND_ME(Method, setFinal, arginfo_class_Method_setFinal, ZEND_ACC_PUBLIC)
+	ZEND_ME(Method, getReflector, arginfo_class_Method_getReflector, ZEND_ACC_PUBLIC)
+	ZEND_FE_END
+};
